f0dd6c4465d0e781ebd8a2811ef42ff5cc06d7a2
Yosefgid <jossykasshun@yahoo.com>
2026-04-08 10:48:26 +0100

    Feat: Add print hello world using async

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Asynchronous
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Asynchronous
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await PrintHelloWorld();
+          
+        }
+        static async Task PrintHelloWorld()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            await Task.Delay(2000);
+            Console.WriteLine("Hello World!");
+            stopwatch.Stop();
+            Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds}ms to execute code!");
         }
     }
 }
