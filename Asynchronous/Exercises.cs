e4a147ca84ee1b9a6d57c470b8667e9d2bba75de
tomlenton <twlenton@gmail.com>
2026-04-08 14:04:28 +0100

    Added print story

---
+++ Asynchronous/Exercises.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous
+{
+    internal class Exercises
+    {
+        public static BigInteger CalculateFactorial(BigInteger num)
+        {
+            BigInteger result = BigInteger.One;
+            for (BigInteger i = BigInteger.One; i.CompareTo(num) <= 0; i = BigInteger.Add(i, BigInteger.One))
+            {
+                result = BigInteger.Multiply(result, i);
+            }
+            return result;
+        }
+    }
+}
--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Net.NetworkInformation;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace Asynchronous
@@ -7,10 +10,41 @@ namespace Asynchronous
     {
         static async Task Main(string[] args)
         {
-            await PrintHelloWorld();
-             
-          
-        }
+            //await PrintHelloWorld();
+
+            string data = "85671 34262 92143 50984 24515 68356 77247 12348 56789 98760";
+            string story = "Mary had a little lamb, its fleece was white as snow.";
+            var words = story.Split(" ");
+
+            List<BigInteger> list = new();
+            list = data.Split(" ").Select(num => BigInteger.Parse(num)).ToList();
+
+            var printStory = Task.Run(async () =>
+            {
+                foreach (var word in words)
+                {
+                    Console.WriteLine(word);
+                    await Task.Delay(1000);
+                }
+            });
+            var factorialNumbers = Task.Run(() =>
+            {
+                var numbersFactorialed = list
+                .Select(num => Exercises.CalculateFactorial(num))
+                .ToList();
+                return numbersFactorialed;
+            }).ContinueWith(t =>
+            {
+                foreach (var numbers in t.Result)
+                {
+                    Console.WriteLine("\n \n \n \n");
+                    Console.WriteLine(numbers);
+                }
+            });
+            //await Task.WhenAll([factorialNumbers, printStory]);
+            
+            }
+        
         static async Task PrintHelloWorld()
         {
             var random = new Random();
